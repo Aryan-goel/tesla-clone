b4b9d34e4dee7a55dbaa01b49bd66a8d3dda27f6
91639 <goel.aryan2406@gmail.com>
2021-11-28 01:55:26 +0530

    added material UI

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
+import MenuIcon from '@mui/icons-material/Menu';
 function header() {
     return (
         <Container>
@@ -7,9 +8,16 @@ function header() {
                 <img src="/images/logo.svg" alt=" "></img>
             </a>
             <Menu>
-
+            <a herf="#">Model S</a>
+            <a herf="#">Model Y</a>
+            <a herf="#">Model 3</a>
+            <a herf="#">Model X</a>
 
             </Menu>
+            <RightMenu>
+            <a herf="#">Shop</a>
+            <a herf="#">Tesla Account</a>
+            </RightMenu>
         </Container>
     )
 }
@@ -18,8 +26,38 @@ export default header
 
 const Container=styled.div`
 min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+padding:0 20px;
+top:0;
+left:0;
+right:0;
+
 
 
 `
 const Menu=styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex:1;
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    padding:0 10px;
+    flex-wrap:nowrap;
+ 
+}
+
+`
+const RightMenu=styled.div`
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    margin-right:10px
+    
+ 
+}
+
 `
