e566555ddd3153002bde58c41b46dbcc946ee454
91639 <goel.aryan2406@gmail.com>
2021-11-27 23:35:56 +0530

    creating header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import styled from "styled-components";
 function header() {
     return (
-        <div>
-            header
-        </div>
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt=" "></img>
+            </a>
+            <Menu>
+
+
+            </Menu>
+        </Container>
     )
 }
 
 export default header
+
+const Container=styled.div`
+min-height:60px;
+
+
+`
+const Menu=styled.div`
+`
