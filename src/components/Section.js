0e1b9912992d53d4a269f8c968981855ef121e1b
91639 <goel.aryan2406@gmail.com>
2021-11-27 13:38:53 +0530

    button designs

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,16 +4,60 @@ import styled from 'styled-components'
 function section() {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                Custom Order
+                </LeftButton>
+                <RightButton>
+                Exsisting Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
 
-export default section
-const Wrap=styled.div`
-width:100vw;
-height:100vh;
-background-size:cover;
+        export default section
+        const Wrap=styled.div`
+        width:100vw;
+        height:100vh;
+        background-size:cover;
+        background-position:center;
+        background-repeat:no-repeat;
+        background-image:url('/images/model-s.jpg');
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+        align-items:center;
+        `
+        const ItemText=styled.div`
+        padding-top:15vh;
+        text-align:center
+        `
+        const ButtonGroup=styled.div`
+        display:flex;
 
-background-image:url('/images/model-s.jpg');
+        `
+const LeftButton=styled.div`
+        background-color:rgba(23,26,32,0.8);
+       height:40px;
+       width:256px;
+       color:white;
+       display:flex;
+       justify-content:center;
+       align-items:center;
+       border-radius:100px;
+        opacity:0.85;
+        text-transform:uppercase;
+        font-size:12px;
+
+
+     
+      
 `
+        const RightButton=styled(LeftButton)`
+
+        `
