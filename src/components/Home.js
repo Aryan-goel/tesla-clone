cf2969b0f68a77937e7bc2f71a94f8bb06b62749
91639 <goel.aryan2406@gmail.com>
2021-11-27 12:20:15 +0530

    images imported

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from "styled-components";
+import Section from './Section';
 
 function home() {
     return (
         <div>
             <Container>
-            Home
+                <Section/>
             </Container>
         </div>
     )
---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function section() {
+    return (
+        <Wrap>
+            Section
+        </Wrap>
+    )
+}
+
+export default section
+const Wrap=styled.div`
+width:100vw;
+height:100vh;
+background-size:cover;
+
+background-image:url('/images/model-s.jpg');
+`
