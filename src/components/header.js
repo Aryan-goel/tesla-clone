b2531925b3fb5070fcccaab727531893996947a0
91639 <goel.aryan2406@gmail.com>
2021-11-29 15:34:25 +0530

    car slice created

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,13 @@ import React,{useState}from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import ClearIcon from "@mui/icons-material/Clear";
+import {selectCars} from '../features/car/carSlice'; 
+import {useSelector} from 'react-redux';
 
 function Header() {
 const[burgerStatus,setBurgerStatus]=useState(false);
+const cars=useSelector(selectCars)
+console.log(cars);
   return (
     <Container>
       <a>
